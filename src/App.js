411ff144a12935613997cd55aa02be791b9fbafb
Lekha iTrace <tech.interviewtrace@gmail.com>
2023-08-20 20:42:11 +0530

    Added build folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  // const [message, setMessage] = useState("");
+
+  // useEffect(() => {
+  //   return () => {
+  //     second;
+  //   };
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
